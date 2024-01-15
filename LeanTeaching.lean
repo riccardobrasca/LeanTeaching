3d92628f5d67acacfe262a50be9f56d01e934102
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-01-15 20:38:57 +0100

    test this

--- LeanTeaching.lean
+++ LeanTeaching.lean
@@ -1,3 +1,5 @@
 -- This module serves as the root of the `LeanTeaching` library.
 -- Import modules here that should be built as part of the library.
-import «LeanTeaching».TD.TD01
+import Mathlib.Tactic
+import Mathlib.Data.Real.Basic
+import Mathlib.Analysis.SpecialFunctions.Trigonometric.Basic
